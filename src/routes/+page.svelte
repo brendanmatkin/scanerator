66887a87820f80cb11fac5a1bc1ae44dc5c5664d
brendanmatkin <brendan.matkin@airdar.com>
2023-12-01 16:34:34 -0700

    add/switch to skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script>
-	import Highlight from 'svelte-highlight';
-	import json from 'svelte-highlight/languages/json';
-	import 'svelte-highlight/styles/github.css';
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+
+	// start highlightjs
+	import hljs from 'highlight.js/lib/core';
+	import json from 'highlight.js/lib/languages/json';
+	hljs.registerLanguage('json', json);
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	storeHighlightJs.set(hljs);
+	//end
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
-<h1>Scanerator Notion Data</h1>
-<Highlight language={json} code={JSON.stringify(data.db, null, 2)} />
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Scanerator Notion Data</h1>
+	<CodeBlock language="json" code={JSON.stringify(data.db, null, 2)} />
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	//https://www.skeleton.dev/docs/purgecss
+	plugins: [sveltekit(), purgeCss({
+		safelist: {
+			greedy: [/^hljs-/],
+		}
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
